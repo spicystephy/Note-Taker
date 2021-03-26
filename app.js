84bf3d501ad1d85c704ba7742902987697ed8611
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-03-26 00:01:12 -0700

    server js updates

---
+++ app.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const { stringify } = require("querystring");
+
+
+async function addNote(note){
+    //read the db json file
+    const notes = await fs.promises.readFile("./db/db.json", "utf8");
+    //parse into js data structure
+    //push to parsed array
+    //stringify data
+    //write to file 
+    await fs.promises.writeFile("./db/db.json", stringifyData);
+}
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const fs = require("fs");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+
 const app = express();
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3000
 
 //middleware functions
 app.use(express.urlencoded({ extended: true }));
@@ -22,10 +25,20 @@ app.get("/notes", function(req, res){
 //api routes
 app.get("/api/notes", function(req, res){
     //gets notes, res.json to the front end, aka getNotes
-})
+    fs.readFile("./db/db.json", "utf8", function (err,data){
+        res.json(JSON.parse(data));
+    });
+});
 app.post("/api/notes", function(req, res){
     //create note from req.body aka saveNote
-})
+    const note = {
+        id: uuidv4(),
+        title: req.body.title,
+        text: req.body.text
+    }
+    console.log(note);
+    res.json(note);
+});
 app.delete("/api/notes/:id", function(req, res){
     //deletes notes based on id aka deleteNote
     const { id } = req.params;
