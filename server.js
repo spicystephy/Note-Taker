09dea6bbd2a7dec2a3bd7aaf760e95e1e9ca7182
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-03-26 00:34:03 -0700

    testing post

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-//middleware functions
+//middleware functions for express to format data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -15,34 +15,35 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //html routes
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-})
-app.get("/notes", function(req, res){
-    res.sendFile(path.join(__dirname, "public", "notes.html"));
-})
-
-//api routes
-app.get("/api/notes", function(req, res){
-    //gets notes, res.json to the front end, aka getNotes
-    fs.readFile("./db/db.json", "utf8", function (err,data){
-        res.json(JSON.parse(data));
-    });
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-app.post("/api/notes", function(req, res){
-    //create note from req.body aka saveNote
-    const note = {
-        id: uuidv4(),
-        title: req.body.title,
-        text: req.body.text
-    }
-    console.log(note);
-    res.json(note);
-});
-app.delete("/api/notes/:id", function(req, res){
-    //deletes notes based on id aka deleteNote
-    const { id } = req.params;
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
+//api routes to get, post and delete data
+app.get("/api/notes", function (req, res) {
+  //gets notes, res.json to the front end, aka getNotes
+  fs.readFile("./db/db.json", "utf8", function (err, data) {
+    res.json(JSON.parse(data));
+  });
+});
+app.post("/api/notes", function (req, res) {
+  //create note from req.body aka saveNote
+  const note = {
+    id: uuidv4(),
+    title: req.body.title,
+    text: req.body.text,
+  };
+//   console.log(note);
+//attach note to 
+//.push?
+  req.body(note);
+});
+app.delete("/api/notes/:id", function (req, res) {
+  //deletes notes based on id aka deleteNote
+  const { id } = req.params;
+});
 
-app.listen(PORT, ()=> console.log("App listening on port " + PORT));
+app.listen(PORT, () => console.log("App listening on port " + PORT));
